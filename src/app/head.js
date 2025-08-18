582ee9cc6d3822fad181daca8a308f167a0f104d
Abdul1013 <abdulhaqabdulrasheed@gmail.com>
2025-08-18 20:00:31 +0100

    updated dependencies

---
+++ src/app/head.js
@@ -0,0 +1,16 @@
+export default function Head() {
+  return (
+    <>
+      <title>Vehiculars - Vehicle Particulars - Easy Installments</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content="Get your vehicle particulars or documents in Nigeria easily by setting up a flexible installment plan on Vehiculars" />
+      <meta name="keywords" content="vehicle particulars, vehicle documents, Nigeria, installment plan, vehiculars" />
+      <meta name="author" content="Transingenium" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
+      <meta name="theme-color" content="#378261" />
+    </>
+  );
+}
