aff9e55b4ae0d4b970ea51e64719f9adb06d4b0b
Abdul1013 <abdulhaqabdulrasheed@gmail.com>
2025-09-06 06:00:45 +0100

    enhanced dl flow

--- src/app/(services)/dl_page/page.js
+++ src/app/(services)/dl_page/page.js
@@ -5,35 +5,37 @@ import ServiceSearch from "@/components/ServiceSearch";
 import ServiceHeader from "@/components/ServiceHeader";
 import ServiceCard from "@/components/ServiceCard";
 import { IdCard } from "lucide-react";
+import { serviceConfig } from "@/config/serviceConfig";
 
-const services = [
-  { id: "fresh-3", name: "Fresh", type: "3 years", price: 38000, icon: IdCard },
-  { id: "fresh-5", name: "Fresh", type: "5 years", price: 45000, icon: IdCard },
-  {
-    id: "renew-3",
-    name: "Renewal",
-    type: "3 years",
-    price: 26000,
-    icon: IdCard,
-  },
-  {
-    id: "renew-5",
-    name: "Renewal",
-    type: "5 years",
-    price: 30000,
-    icon: IdCard,
-    uploadRequired: 1,
-    docName: ["current driver license"],
-    fields: [],
-  },
-  {
-    id: "intl-1",
-    name: "International (IDP)",
-    duration: "1 year",
-    price: 25000,
-    icon: IdCard,
-  },
-];
+const services = serviceConfig.driverLicense
+// const services = [
+//   { id: "fresh-3", name: "Fresh", type: "3 years", price: 38000, icon: IdCard },
+//   { id: "fresh-5", name: "Fresh", type: "5 years", price: 45000, icon: IdCard },
+//   {
+//     id: "renew-3",
+//     name: "Renewal",
+//     type: "3 years",
+//     price: 26000,
+//     icon: IdCard,
+//   },
+//   {
+//     id: "renew-5",
+//     name: "Renewal",
+//     type: "5 years",
+//     price: 30000,
+//     icon: IdCard,
+//     uploadRequired: 1,
+//     docName: ["current driver license"],
+//     fields: [],
+//   },
+//   {
+//     id: "intl-1",
+//     name: "International (IDP)",
+//     duration: "1 year",
+//     price: 25000,
+//     icon: IdCard,
+//   },
+// ];
 
 export default function DriverLicensePage() {
   const [query, setQuery] = useState("");
@@ -65,20 +67,6 @@ export default function DriverLicensePage() {
 
       {/* Services Grid */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-        {/* {filtered.map((service) => (
-          <div
-            key={service.id}
-            onClick={() => handleSelect(service)}
-            className="cursor-pointer bg-gray-50 border rounded-xl p-6 shadow hover:shadow-lg transition flex flex-col items-center text-center"
-          >
-            <Image src={"/icons/dl.png"} width={50} height={50} alt="id_card"/>
-            <h3 className="font-semibold text-lg">{service.name}</h3>
-            <p className="text-sm text-gray-500">{service.duration}</p>
-            <p className="text-xl font-bold text-green-700 mt-3">
-              ₦{service.price.toLocaleString()}
-            </p>
-          </div>
-        ))} */}
         {filteredServices.map((service) => (
           <ServiceCard
             key={service.id}
